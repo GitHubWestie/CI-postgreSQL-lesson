43726efa647e5f90b5d3e5ca3fbd778920128df6
Keiran West <127797224+GitHubWestie@users.noreply.github.com>
2024-06-04 11:15:04 +0000

    updated psycopg2.py

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -7,8 +7,32 @@ connection = psycopg2.connect(database="chinook")
 # build a cursor object of the database
 cursor = connection.cursor()
 
-#Query 1 - select all records from the artist table
-cursor.execute('SELECT * FROM "Artist"')
+# Query 1 - select all records from the artist table
+# cursor.execute('SELECT * FROM "Artist"')
+
+# Query 2 - select only the "Name" column from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# Query 3 - select only "Queen" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# Query 4 - select only by artist id #51 from the artist table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# Query 5 - select only the albums with artistId #51 on the album table
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# CHallenge
+
+# Query 7 - select all tracks from the composer table
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Philip Glass"])
+
+# Query 8 - Use "test" as a composer
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["test"])
+
 
 # fetch the results (multiple)
 results = cursor.fetchall()
